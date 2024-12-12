980e4a453124f2466d509c17a83e5fb885078e95
Olga Kazorina <nefertituolga@gmail.com>
2024-12-12 20:44:35 +0300

    add filter_by_state function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from src.processing import filter_by_state
 from src.widget import get_date, mask_account_widget
 
 if __name__ == "__main__":
@@ -8,3 +9,8 @@ if __name__ == "__main__":
     print()
 
     print(get_date("2024-03-11T02:26:18.671407"))
+    print()
+
+    list = [{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+    print(filter_by_state(list, state="CANCELED"))
