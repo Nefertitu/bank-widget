12a5ae5569225bf3ca26fec60703755ff7846066
Olga Kazorina <nefertituolga@gmail.com>
2024-12-14 14:05:49 +0300

    add new fixes main.py, src\processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,7 @@
-def filter_by_state(list_dictionaries: list[dict[str, str]], state: str = "EXECUTED") -> list[dict[str, str]]:
+from typing import Any
+
+
+def filter_by_state(list_dictionaries: list[dict[str, Any]], state: str = "EXECUTED") -> list[dict[str, Any]]:
     """Фильтрует список словарей с данными о банковских операциях по параметру state"""
     new_list_dictionaries = []
     for dictionary in list_dictionaries:
@@ -9,7 +12,7 @@ def filter_by_state(list_dictionaries: list[dict[str, str]], state: str = "EXECU
     return new_list_dictionaries
 
 
-def sort_by_date(list_dictionaries: list[dict[str, str]], sort_order: bool = True) -> list[dict[str, str]]:
+def sort_by_date(list_dictionaries: list[dict[str, Any]], sort_order: bool = True) -> list[dict[str, Any]]:
     """Сортирует полученный список словарей по дате, параметр, задающий порядок сортировки, по умолчанию - убывание"""
     if sort_order is True:
         sorted_dictionaries = sorted(list_dictionaries, key=lambda dictionary: dictionary["date"], reverse=True)
